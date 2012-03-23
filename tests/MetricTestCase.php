fb32996781bf9db92264204f1539e0932175adf9
till <till@php.net>
2012-03-23 20:06:44 +0100

    cleanup

--- tests/MetricTestCase.php
+++ tests/MetricTestCase.php
@@ -7,9 +7,10 @@ class MetricTestCase extends \PHPUnit_Framework_TestCase
 {
     public function testMetric()
     {
-        $metric = new \Services\Librato\Metrics\Metric('foo');
-        $metric->value  = 10;
-        $metric->period = '10s';
+        $metric               = new Metric('foo');
+        $metric->value        = 10;
+        $metric->period       = '10s';
+        $metric->measure_time = 'ts';
 
         $this->assertEquals('{"name":"foo","value":10,"period":"10s"}', $metric->toJson());
     }
