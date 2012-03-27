787a33d9382da5e13229b2a587b4530861b29c37
till <till@php.net>
2012-03-27 13:59:46 +0200

    finish collection base class for counters and gauges

--- library/Services/Librato/Metrics/Collection.php
+++ library/Services/Librato/Metrics/Collection.php
@@ -1,12 +1,45 @@
 <?php
-namespace \Services\Librato\Metrics;
-
-use \Services\Librato\Metrics\Metric;
+namespace Services\Librato\Metrics;
 
 /**
  * @author Till Klampaeckel <till@php.net>
  */
-class Collection
+abstract class Collection
 {
+    /**
+     * An Array of {@link \Services\Librato\Metrics\Metric}
+     * @var array
+     */
+    protected $metrics;
 
+    public function accept(Metric $metric)
+    {
+        if ($this->metrics === null) {
+            $this->metrics = array();
+        }
+        $this->metrics[] = $metric;
+        return $this;
+    }
+
+    /**
+     * Get a JSON-encoded string of metrics.
+     *
+     * @return string
+     */
+    public function toJson()
+    {
+        $type = str_replace(
+            'services\librato\metrics\\',
+            '',
+            strtolower(get_class($this))
+        );
+
+        $response        = array();
+        $response[$type] = array();
+
+        foreach ($this->metrics as $metric) {
+            $response[$type][] = $metric->toArray();
+        }
+        return json_encode($response);
+    }
 }
