228c543c1169efd121264e3b49f5454c1fa07230
till <till@php.net>
2012-03-27 13:59:57 +0200

    counters and gauges

---
+++ library/Services/Librato/Metrics/Counters.php
@@ -0,0 +1,9 @@
+<?php
+namespace Services\Librato\Metrics;
+
+/**
+ * @author Till Klampaeckel <till@php.net>
+ */
+class Counters extends Collection
+{
+}
---
+++ library/Services/Librato/Metrics/Gauges.php
@@ -0,0 +1,9 @@
+<?php
+namespace Services\Librato\Metrics;
+
+/**
+ * @author Till Klampaeckel <till@php.net>
+ */
+class Gauges extends Collection
+{
+}
