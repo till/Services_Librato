6b43d71ed0f1350f01c299854c1afc05631a13d3
till <till@php.net>
2012-03-27 16:05:25 +0200

    fix path to loader

--- tests/TestInit.php
+++ tests/TestInit.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/library/Services/Librato/Autoloader.php';
+require_once dirname(__DIR__) . '/library/Services/Librato/Autoloader.php';
 
 $loader = new \Services\Librato\Autoloader();
 $loader->register();
