dcc118bfdeac58567a1a2176a0bee0e80c7f1d25
till <till@php.net>
2012-03-27 16:22:28 +0200

    test

---
+++ tests/Services/Librato/Test/Metrics/SuperCollectionTestCase.php
@@ -0,0 +1,34 @@
+<?php
+namespace Services\Librato\Test\Metrics;
+
+use \Services\Librato\Metrics\Metric;
+use \Services\Librato\Metrics\Counters;
+use \Services\Librato\Metrics\Gauges;
+use \Services\Librato\Metrics\SuperCollection;
+
+class SuperCollectionTestCase extends \PHPUnit_Framework_TestCase
+{
+    public function testToArray()
+    {
+        $counter               = new Metric('example.counter');
+        $counter->value        = 1;
+        $counter->measure_time = 1332848921;
+
+        $counters = new Counters();
+        $counters->accept($counter);
+
+        $gauge               = new Metric('some.gauge');
+        $gauge->value        = 1;
+        $gauge->measure_time = 1332848921;
+
+        $gauges = new Gauges();
+        $gauges->accept($gauge);
+
+        $superCollection = new SuperCollection;
+        $superCollection->accept($gauges)->accept($counters);
+
+        $payLoad = $superCollection->toArray();
+        var_dump(json_encode($payLoad));
+
+    }
+}
