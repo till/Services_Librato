af1cd35d0665aab48c1c1cf1b7bddaa7d886c8a6
till <till@php.net>
2012-03-27 13:52:15 +0200

    * test case for our collection stuff

---
+++ tests/Services/Librato/Test/Metrics/CollectionTestCase.php
@@ -0,0 +1,38 @@
+<?php
+namespace Services\Librato\Test\Metrics;
+
+use \Services\Librato\Metrics\Metric;
+use \Services\Librato\Metrics\Counters;
+
+class CollectionTestCase extends \PHPUnit_Framework_TestCase
+{
+    public function testCounter()
+    {
+        $counter               = new Metric('example.counter');
+        $counter->value        = 1;
+        $counter->measure_time = 1332848921;
+
+        $collection = new Counters();
+        $collection->accept($counter);
+
+        $this->assertEquals(
+            '{"counters":[{"name":"example.counter","value":1,"measure_time":1332848921}]}', 
+            $collection->toJson()
+        );
+    }
+
+    public function testGauges()
+    {
+        $gauge               = new Metric('some.gauge');
+        $gauge->value        = 1;
+        $gauge->measure_time = 1332848921;
+
+        $collection = new Gauges();
+        $collection->accept($gauge);
+
+        $this->assertEquals(
+            '{"gauges":[{"name":"some.gauge","value":1,"measure_time":1332848921}]}', 
+            $collection->toJson()
+        );
+    }
+}
