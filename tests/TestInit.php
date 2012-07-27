e4aa6e6075c914fc72801e2856903a27a03f67a9
till <till@php.net>
2012-07-27 17:30:53 +0200

    Add autoload definition. Add HTTP_Request2 as a dependency. Use autoloader from Composer when available.

--- tests/TestInit.php
+++ tests/TestInit.php
@@ -1,5 +1,10 @@
 <?php
-require_once dirname(__DIR__) . '/library/Services/Librato/Autoloader.php';
+$rootDir = dirname(__DIR__);
 
-$loader = new \Services\Librato\Autoloader();
-$loader->register();
+if (file_exists($rootDir . '/vendor/autoload.php')) {
+    require $rootDir . '/vendor/autoload.php';
+} else {
+    require_once dirname(__DIR__) . '/library/Services/Librato/Autoloader.php';
+    $loader = new \Services\Librato\Autoloader();
+    $loader->register();
+}
